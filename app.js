d122b4c4dd49e40f74178a56cc2d8cdcf4412dce
Manish Kumar <manishk68806@gmail.com>
2025-04-05 05:28:17 +0530

    login api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express=require('express');
 const dbConnection=require('./models/dbConnection');
 const bcrypt = require('bcrypt');
 const userInfoModel=require('./models/Model');
+const jwt=require('jsonwebtoken');
 
 const app=express();
 
@@ -13,7 +14,7 @@ app.post('/signup',async(req,res,next)=>{
     if(!Validation(emailId,password,name,avatar)){
         const error=new Error('Invalid Credentials');
         error.statusCode=400;
-        throw error;
+        next(error);
     }
     try{
     const encryptedPassword=await bcrypt.hash(password,saltRounds);
@@ -23,12 +24,52 @@ app.post('/signup',async(req,res,next)=>{
         return res.redirect('/login');
     }
     const data=new userInfoModel({emailId,password:encryptedPassword,name,avatar});
-    await data.save()}
+    await data.save();
+    res.status(201).json({message:"SignUp successfull"});
+}
     catch(e){
         const error=new Error('Internal Server Error');
         error.statusCode=500;
-        throw error;
+        return next(error);
     }
+})
+
+app.post('/login',async(req,res,next)=>{
+    try{
+    const{emailId,password}=req.body;
+    if(!Validation(emailId,password)){
+        const error=new Error('Invalid Credentials');
+        error.statusCode=400;
+        return next(error);
+    }
+    const data = await userInfoModel.findOne({emailId:emailId});
+    if(!data){
+        const error=new Error('Wrong credentials');
+        error.statusCode=400;
+        return next(error);
+    }
+    const check = await bcrypt.compare(password,data.password)
+    if(!check){
+        const error=new Error('Wrong Credentials');
+        error.statusCode=400;
+        return next(error);
+    };
+    const secretKey=process.env.SECRET_KEY;
+    const jwtToken=jwt.sign({userId:data._id},secretKey,{expiresIn:'7d'})
+    res.cookie('token',jwtToken,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV==='production',
+        maxAge:7*24*60*60*1000,
+        sameSite:'strict',
+    })
+    res.status(201).json({message:'login successfully'});
+}
+    catch(err){
+        const error=new Error('Internal Server Error');
+        error.statusCode=500;
+        return next(error);
+    }
+
 })
 
 
